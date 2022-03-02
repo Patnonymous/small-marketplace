8bd4ec9f7e84ce1456618dd4ee63e38a8f801685
Patrick_Williamson <patrickw9080@outlook.com>
2022-03-02 14:59:11 -0500

    Updated app title.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="container">
-    <div class="row">
-      <div class="col">
+    <div class="row justify-content-center">
+      <div class="col-auto text-center">
         <h1>Ye Olde Small Market</h1>
+        <span
+          >"Your one stop shop for non existent products since yesterday."</span
+        >
       </div>
     </div>
   </div>
