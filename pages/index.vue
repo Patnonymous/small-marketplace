e2d328ffef73ac751a873b0d52ecd7120b83a633
Patrick_Williamson <patrickw9080@outlook.com>
2022-03-02 14:53:15 -0500

    Added default layout. Removed tutorial.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,15 @@
 <template>
-  <Tutorial/>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Ye Olde Small Market</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
-export default Vue.extend({})
+export default Vue.extend({});
 </script>
